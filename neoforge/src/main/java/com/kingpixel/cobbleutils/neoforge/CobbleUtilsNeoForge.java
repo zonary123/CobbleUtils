42f802abd3d4f1eccb44df08931b9e6c9161b85d
zonary123 <carlosvarasalonso10@gmail.com>
2025-01-02 23:48:44 +0100

    v1.1.3 Neoforge

--- neoforge/src/main/java/com/kingpixel/cobbleutils/neoforge/CobbleUtilsNeoForge.java
+++ neoforge/src/main/java/com/kingpixel/cobbleutils/neoforge/CobbleUtilsNeoForge.java
@@ -1,11 +1,13 @@
 package com.kingpixel.cobbleutils.neoforge;
 
 import com.kingpixel.cobbleutils.CobbleUtils;
+import net.neoforged.bus.api.IEventBus;
 import net.neoforged.fml.common.Mod;
 
-@Mod(CobbleUtils.MOD_ID) public final class CobbleUtilsNeoForge {
-  public CobbleUtilsNeoForge() {
-    // Run our common setup.
+@Mod(CobbleUtils.MOD_ID)
+public class CobbleUtilsNeoForge {
+
+  public CobbleUtilsNeoForge(IEventBus modBus) {
     CobbleUtils.init();
   }
 }
