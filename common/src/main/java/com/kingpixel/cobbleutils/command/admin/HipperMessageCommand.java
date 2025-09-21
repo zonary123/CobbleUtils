c3bf5e98492698c69d0349babd8120dc0dbe1a78
zonary123 <carlosvarasalonso10@gmail.com>
2025-09-22 00:59:11 +0200

    Refactor HipperMessageCommand; rename command to sendMessage and add message suggestions for improved user experience

--- common/src/main/java/com/kingpixel/cobbleutils/command/admin/HipperMessageCommand.java
+++ common/src/main/java/com/kingpixel/cobbleutils/command/admin/HipperMessageCommand.java
@@ -5,26 +5,34 @@ import com.kingpixel.cobbleutils.Model.messages.HiperMessage;
 import com.mojang.brigadier.CommandDispatcher;
 import com.mojang.brigadier.arguments.StringArgumentType;
 import com.mojang.brigadier.builder.LiteralArgumentBuilder;
+import net.minecraft.command.CommandSource;
 import net.minecraft.server.command.CommandManager;
 import net.minecraft.server.command.ServerCommandSource;
 import net.minecraft.server.network.ServerPlayerEntity;
-
-import java.util.UUID;
 
 /**
  * @author Carlos Varas Alonso - 07/11/2024 4:18
  */
 public class HipperMessageCommand {
-  private static final UUID ZONARY_UUID = UUID.fromString("239643a3-0b4d-40d5-bea1-f8814ba536ef");
-  private static final String ZONARY_NAME = "zonary123";
 
   public static void register(CommandDispatcher<ServerCommandSource> dispatcher, LiteralArgumentBuilder<ServerCommandSource> base) {
     dispatcher.register(
       base.then(
-        CommandManager.literal("hipper")
+        CommandManager.literal("sendMessage")
           .requires(source -> source.hasPermissionLevel(2))
           .then(
             CommandManager.argument("message", StringArgumentType.greedyString())
+              .suggests(((context, builder) -> CommandSource.suggestMatching(new String[]{
+                "chat: Hello %player:name%!",
+                "chat_broadcast: Hello everyone!",
+                "actionbar: Hello %player:name%!",
+                "actionbar_broadcast: Hello everyone!",
+                "titlesubtitle:title: Hello %player:name%! subtitle: This is a subtitle example.",
+                "titlesubtitle_broadcast:title: Hello everyone! subtitle: This is a subtitle example.",
+                "bossbar: Hello %player:name%!",
+                "bossbar_broadcast: Hello everyone!",
+                "chat: Hello %player:name%! sound:entity.experience_orb.pickup volume:1.0 pitch:1.0",
+              }, builder)))
               .executes(context -> {
                 String message = StringArgumentType.getString(context, "message");
                 ServerPlayerEntity player = null;
