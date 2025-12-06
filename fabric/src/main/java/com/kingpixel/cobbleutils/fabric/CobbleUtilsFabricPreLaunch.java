382dd07c08b6c5b2508783b3da1594542699b30e
MemencioPerez <memencio.perez.2004@gmail.com>
2025-12-06 18:52:57 -0400

    Update libby-fabric to 2.0.0

--- fabric/src/main/java/com/kingpixel/cobbleutils/fabric/CobbleUtilsFabricPreLaunch.java
+++ fabric/src/main/java/com/kingpixel/cobbleutils/fabric/CobbleUtilsFabricPreLaunch.java
@@ -9,14 +9,12 @@ import org.slf4j.LoggerFactory;
 import java.nio.file.Path;
 
 public class CobbleUtilsFabricPreLaunch implements PreLaunchEntrypoint {
-    private static final String MOD_ID = "cobbleutils";
-    private static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
-    private static final Path DATA_DIRECTORY = FabricLoader.getInstance().getConfigDir().resolve(MOD_ID);
+  private static final String MOD_ID = "cobbleutils";
+  private static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
-    @Override
-    public void onPreLaunch() {
-        var modContainer = FabricLoader.getInstance().getModContainer(MOD_ID).orElseThrow();
-        FabricLibraryManager libraryManager = new FabricLibraryManager(LOGGER, DATA_DIRECTORY, modContainer);
-        libraryManager.configureFromJSON();
-    }
+  @Override
+  public void onPreLaunch() {
+    FabricLibraryManager libraryManager = new FabricLibraryManager(LOGGER, MOD_ID);
+    libraryManager.configureFromJSON();
+  }
 }
