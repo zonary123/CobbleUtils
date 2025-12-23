70b9f86c41fd8809613ad6f5597be512ffc70c60
zonary123 <carlosvarasalonso10@gmail.com>
2025-12-23 01:18:46 +0100

    Update invisible item model from paper to air for improved functionality

--- common/src/main/java/com/kingpixel/cobbleutils/config/Lang.java
+++ common/src/main/java/com/kingpixel/cobbleutils/config/Lang.java
@@ -258,7 +258,7 @@ public class Lang {
     movecolor.put("ground", "<gradient:#B8860B:#D2B48C>");
     movecolor.put("psychic", "<gradient:#D74DE2:#DE77E7>");
     movecolor.put("fairy", "<gradient:#9C38A5:#C06EC7>");
-    itemInvisible = new ItemModel("minecraft:paper", " ", List.of(), 1);
+    itemInvisible = new ItemModel("minecraft:air", " ", List.of(), 1);
     itemMoney = new ItemModel("cobblemon:relic_coin", "%amount%", List.of(), 0);
     itemPc = new ItemModel("cobblemon:pc", "&bPC", List.of(
       "&7Right click to open"
